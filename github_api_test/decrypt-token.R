ba2aa5e27951fd5bbaf45d0a94ec4630e077404b
liao <liaomovie2@gmail.com>
2019-01-12 14:42:26 +0800

    up

--- github_api_test/decrypt-token.R
+++ github_api_test/decrypt-token.R
@@ -2,9 +2,15 @@ decrypt <- function(password) {
   rcrypt::decrypt('encrypted.gpg', 
                   output = 'temp', 
                   passphrase = password)
+  if (!file.exists('encrypted.gpg')) {
+    stop('No encrypted file')
+  }
   if (file.exists('temp')) {
     token <- readLines('temp')
     file.remove('temp')
     return(token)
-  } else return(100)
+  } else {
+    warning('Wrong Password')
+    return(NULL)
+  }
 }
